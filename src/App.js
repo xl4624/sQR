00856b390dabf0e1b3b8b1d9101b22adf2501b7a
Xiaomin Liu <xl4624@nyu.edu>
2025-02-08 15:02:36 -0500

    Get scanned URLs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { useEffect, useRef, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import QrScanner from 'qr-scanner';
 
 function App() {
   const videoRef = useRef(null);
+  const [qrScanner, setQrScanner] = useState(null);
+  const [result, setResult] = useState('');
 
   async function startScanner() {
+    if (qrScanner) {
+      await qrScanner.start();
+      return;
+    }
     const scanner = new QrScanner(
       videoRef.current,
-      (result) => console.log('decoded qr code:', result),
+      (result) => {
+        console.log('decoded qr code:', result.data);
+        setResult(result.data);
+      },
       {
         returnDetailedScanResult: true,
         highlightScanRegion: true,
         highlightCodeOutline: true,
       }
     );
+
+    setQrScanner(scanner);
     await scanner.start();
-  }
+  };
+
+  function stopScanner() {
+    if (qrScanner) {
+      qrScanner.stop();
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,6 +42,12 @@ function App() {
         <button onClick={startScanner}>
           Start Scanner
         </button>
+        <button onClick={stopScanner}>
+          Stop Scanner
+        </button>
+        {result && (
+          <div>Scanned Result: {result}</div>
+        )}
       </header>
     </div>
   );

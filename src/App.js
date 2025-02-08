9fec8517ab325490fcfee30e47767befa7f173a9
Xiaomin Liu <xl4624@nyu.edu>
2025-02-08 15:44:03 -0500

    Add url history

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import QrScanner from 'qr-scanner';
 
+const GUEST_URL_HISTORY_SIZE = 3;
+
 function App() {
   const videoRef = useRef(null);
   const [qrScanner, setQrScanner] = useState(null);
   const [result, setResult] = useState('');
+  const urlCacheRef = useRef(new Set());
+  const [urlHistory, setUrlHistory] = useState([]);
+
+  useEffect(() => {
+    const saved = localStorage.getItem('history');
+    if (saved) {
+      urlCacheRef.current = new Set(JSON.parse(saved));
+      setUrlHistory(Array.from(urlCacheRef.current));
+    }
+  }, []);
+
+  function addToHistory(url) {
+    const set = urlCacheRef.current;
+    set.delete(url);
+    if (set.size >= GUEST_URL_HISTORY_SIZE) {
+      set.delete(Array.from(set)[0]);
+    }
+    set.add(url);
+    // History table should be in reverse chronological order.
+    const newUrls = Array.from(set).reverse();
+    setUrlHistory(newUrls);
+    localStorage.setItem('history', JSON.stringify(newUrls));
+  }
 
   async function startScanner() {
     if (qrScanner) {
@@ -17,6 +42,7 @@ function App() {
       (result) => {
         console.log('decoded qr code:', result.data);
         setResult(result.data);
+        addToHistory(result.data);
       },
       {
         returnDetailedScanResult: true,
@@ -47,6 +73,21 @@ function App() {
         </button>
         {result && (
           <div>Scanned Result: {result}</div>
+        )}
+        {urlHistory && urlHistory.length > 0 && (
+          <div>
+            <br /><br /><br /><br />
+            <table>
+              <thead>
+                <tr><th>URL</th></tr>
+              </thead>
+              <tbody>
+                {urlHistory.map((url) => (
+                  <tr key={url}><td>{url}</td></tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         )}
       </header>
     </div>

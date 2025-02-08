1e76a16e128206e01eac91619e46afbdf4fc68c4
Xiaomin Liu <xl4624@nyu.edu>
2025-02-08 16:13:41 -0500

    Retrigger Vercel deployment

--- api/app.py
+++ api/app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 app.register_blueprint(scan_bp)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return "Hello World"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- api/routes/scan.py
+++ api/routes/scan.py
@@ -6,21 +6,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-scan_bp = Blueprint('scan', __name__)
+scan_bp = Blueprint("scan", __name__)
 
-SAFE_BROWSING_API_KEY = os.getenv('API_KEY')
+SAFE_BROWSING_API_KEY = os.getenv("API_KEY")
 print(SAFE_BROWSING_API_KEY)
 if not SAFE_BROWSING_API_KEY:
     raise ValueError("API_KEY is not set")
-SAFE_BROWSING_URL = "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=" + SAFE_BROWSING_API_KEY
+SAFE_BROWSING_URL = (
+    "https://safebrowsing.googleapis.com/v4/threatMatches:find?key="
+    + SAFE_BROWSING_API_KEY
+)
 
-@scan_bp.route('/scan', methods=['POST'])
+
+@scan_bp.route("/scan", methods=["POST"])
 def scan():
     data = request.get_json()
-    url = data.get('URL')
+    url = data.get("URL")
     if not url:
         return jsonify({"error": "URL is required"}), 400
-    
+
     check_url_db = get_url(url)
     print(check_url_db)
     if not check_url_db:
@@ -28,26 +32,26 @@ def scan():
         insert_url(url, result)
         return jsonify({"URL": url, "result": result})
     ## subject, description, reports
-    return jsonify({"URL": url, "result": check_url_db['result']})
+    return jsonify({"URL": url, "result": check_url_db["result"]})
 
 
-    
-    
-    
 def check_url_safety(url):
     payload = {
-        "client": {
-            "clientId": "your-client-id",
-            "clientVersion": "1.0"
-        },
+        "client": {"clientId": "your-client-id", "clientVersion": "1.0"},
         "threatInfo": {
-            "threatTypes": ["MALWARE", "SOCIAL_ENGINEERING", "UNWANTED_SOFTWARE", "POTENTIALLY_HARMFUL_APPLICATION"],
+            "threatTypes": [
+                "MALWARE",
+                "SOCIAL_ENGINEERING",
+                "UNWANTED_SOFTWARE",
+                "POTENTIALLY_HARMFUL_APPLICATION",
+            ],
             "platformTypes": ["ANY_PLATFORM"],
             "threatEntryTypes": ["URL"],
-            "threatEntries": [{"url": url}]
-        }
+            "threatEntries": [{"url": url}],
+        },
     }
 
     response = requests.post(SAFE_BROWSING_URL, json=payload)
     data = response.json()
     return "safe" if "matches" not in data else "malicious"
+
